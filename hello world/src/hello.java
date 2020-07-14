f71ad7e2f91abe637b6495311c425b03d735cf1b
k001 <k001@NB308.coinor.local>
2020-07-14 18:14:11 +0200

    erste uebung

---
+++ hello world/src/hello.java
@@ -0,0 +1,29 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		// Ausgabe Hello World!
+		String hallo = "hello world!";
+
+		Integer n = 4;
+		String hallo2 = "hallo der Zweite!" + " Und ich der " + n + ".";
+		System.out.println(hallo2);
+
+		for (int i = 0; i < 1000; i++) {
+			String hallo3 = " ...und ich der " + i + ".";
+			System.out.println(hallo3);
+
+		}
+
+		Integer IGOR = 2147483647 + 10; // ich bin nicht so schlau;
+		System.out.println(IGOR);
+
+		Double DGOR = 26.9;
+		System.out.println(DGOR);
+
+		Byte bvar = (120);
+		System.out.println(bvar + 123);
+
+	}
+
+}
